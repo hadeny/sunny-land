3cfab6ed1fb92f20452fff8032bf29c2e9c5922e
pingpong <khd9545@gmail.com>
2020-10-23 17:52:57 +0900

    10/23- first
    슬라이드 오류수정, 사진추가

--- port/script/portList.js
+++ port/script/portList.js
@@ -2,7 +2,7 @@ let slideSection = $(".list02"); //section.list02 부분만 슬라이드쇼
 let goImg = slideSection.find("img"); 
 
 let now = 0;
-let imgs = 1; //이미지갯수 -1
+let imgs = 3; //이미지갯수 -1
 let matchV = 0;
 
 slideStart();
@@ -14,7 +14,7 @@ function slideStart() {
 function slideGo() {
     now = now ==imgs ? now=0: ++now;
     $(goImg).eq(now-1).css({ //밑에서 위로 이동
-        "margin-top" : "-44vh",
+        "margin-top" : "-43vh",
         "transition" : "all 1s"
     });
     $(goImg).eq(now).css({
@@ -22,30 +22,30 @@ function slideGo() {
         "transition" : "all 1s"
     });
 
-    // for (let i =0; i <=imgs; i++) {
-    //     matchV++;
-    //     if(i ==imgs) matchV =0;
-    //     if(now==i) {
-    //         $(goImg).eq(matchV).css({
-    //             "margin-top":"44vh",
-    //             "transition":"none"
-    //         });
-    //     }
-    // } 왜안될까
-
-    if(now==0) {
-        $(goImg).eq(1).css({
-            "margin-top" : "44vh",
-            "transition" : "none"
-        });
+    for (let i =0; i <=imgs; i++) {
+        matchV++;
+        if(i ==imgs) matchV =0;
+        if(now==i) {
+            $(goImg).eq(matchV).css({
+                "margin-top":"43vh",
+                "transition":"none"
+            });
+        }
     }
 
-    if(now==1) {
-        $(goImg).eq(0).css({
-            "margin-top" : "44vh",
-            "transition" : "none"
-        });
-    }
+    // if(now==0) {
+    //     $(goImg).eq(1).css({
+    //         "margin-top" : "44vh",
+    //         "transition" : "none"
+    //     });
+    // }
+
+    // if(now==1) {
+    //     $(goImg).eq(0).css({
+    //         "margin-top" : "44vh",
+    //         "transition" : "none"
+    //     });
+    // }
 
 }
 
